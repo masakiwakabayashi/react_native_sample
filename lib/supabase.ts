5edfe556138a53607a79d897a21a027bb9cbde6a
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-05-29 20:26:29 +0900

    fix supabase error

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,9 +4,4 @@ const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-// TODO: ここのエラーは解消する
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  localStorage: AsyncStorage as any,
-  detectSessionInUrl: false // Prevents Supabase from evaluating window.location.href, breaking mobile
-});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
